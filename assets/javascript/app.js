0ec7476255d09a261651b950472e27412331b179
gabaly <Gabaly@Mohameds-MacBook-Pro-2.local>
2019-04-16 15:28:57 -0400

    small modifications

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@
 // Counters
 var correctanswer = 0;
 var wronganswer = 0;
+var unanswered =0;
 
 // Options and Answers
 
@@ -11,28 +12,75 @@ var wronganswer = 0;
 
 $(document).ready(function () {
 
+    $('.GameQuestion').hide()
+    $('.start').show()
+    $('.results').hide()
 
-    $("#submit").click(function () {
+
+    // Game ending
+
+    $("#submit").click(endgame)
+    $('.start').click(start)
+    $('#restart').click(restart)
+
+
+
+
+
+
+
+    // Game Function
+    function endgame() {
 
         var radioVList = [$("input[name='inlineRadioOptions1']:checked").val(), $("input[name='inlineRadioOptions2']:checked").val(), $("input[name='inlineRadioOptions3']:checked").val(), $("input[name='inlineRadioOptions4']:checked").val(), $("input[name='inlineRadioOptions5']:checked").val()]
         var Answer = ["option3", "option3", "option2", "option1", "option2"]
 
-        for (i = 0; i < radioVList.length; i++)
+        for (i = 0; i < radioVList.length; i++) {
             console.log(radioVList[i])
-        var radioValue = radioVList[i];
-        var answerValue = Answer[i];
-        if (radioValue === answerValue) {
-            correctanswer++
+            var radioValue = radioVList[i];
+            var answerValue = Answer[i];
+            if (radioValue === answerValue) {
+                correctanswer++
+            }
+            else if (radioValue === undefined) {
+                unanswered++
+            }
+            else {
+                wronganswer++
+            }
+
         }
-        else if (radioValue === "undefined"){
-            wronganswer++
-        }
-        else {
-            wronganswer++
-        }
-        console.log(correctanswer)
-        console.log(wronganswer)
-    })
+
+        $('.GameQuestion').hide()
+        $('.results').show()
+        $('#answers-right').html("Number of Correct Answers = " + correctanswer)
+        $('#answers-wrong').html("Number of Wrong Answers = " + wronganswer)
+        $('#unanswered').html("Number of unanswered questions = " + unanswered)
+        
+    }
+
+    // start button
+    function start() {
+        $('.start').hide()
+        $('.GameQuestion').show()
+        setTimeout(endgame, 25000)
+
+        // timer 
+        var timeleft = 25;
+        var downloadTimer = setInterval(function () {
+            $('#display').html(timeleft + " seconds remaining") 
+            console.log(timeleft + " seconds remaining")
+            timeleft -= 1;
+            if (timeleft < 0) {
+                clearInterval(downloadTimer);
+            }
+        }, 1000);
+    }
+
+    //restart
+    function restart() {
+        location.reload();
+    }
 
 
 
